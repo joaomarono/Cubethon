cafe27fc86a359bb876d549c7bebdb24efc161e0
Joao Marono <the_real_kazan@protonmail.com>
2020-09-29 11:27:30 +0100

    First Game
    This is my first game in unity.

---
+++ MyFirstGame/Assets/Scripts/EndScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+    public GameManager gameManager;
+
+    private void OnTriggerEnter()
+    {
+        gameManager.completeLevel();
+    }
+}
---
+++ MyFirstGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float movingForward = 500f;
+    public float movingSides = 500f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, movingForward * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(movingSides * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-movingSides * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if(rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
